cb1537ccab9d5f08531d06afb630f499c91f8e7c
Lewis J Hannah <lewis.j.hannah@gmail.com>
2024-11-28 12:06:13 -0500

    Refactored code to separate data models from api project

---
+++ OrbTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http.Headers;
+using OrbDataModels;
+using OrbDataModels.BLL;
+
+string connectionString = "Server=localhost;Database=orb_db;User ID=lewisjhannah;Password=precious5;Port=3306;";
+
+ResourceBLL resourceBLL = new ResourceBLL(connectionString);
+
+Console.WriteLine("List of States:");
+var states = resourceBLL.GetAllStates();
+foreach (var state in states)
+{
+    Console.WriteLine(state.StateName);
+}
