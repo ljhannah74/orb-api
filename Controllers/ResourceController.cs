2b666bd9fac53c4bb566b32e003fac0a1864b74d
Lewis J. Hannah <lewis.j.hannah@gmail.com>
2025-01-13 12:13:24 -0500

    Refactored some of the API endpoints Removed the bitbucket-pipelines.yml file

--- Controllers/ResourceController.cs
+++ Controllers/ResourceController.cs
@@ -21,7 +21,7 @@ namespace orb_api.Controllers
             return Ok(new string[] { "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY" });
         }
 
-        [HttpGet("states/{state}/counties")]
+        [HttpGet("counties/{state}")]
         public ActionResult<IEnumerable<string>> GetCounties(string state)
         {
             var counties = _context.Orbs.Where(c => c.State == state).Select(c => c.County).ToList();
--- Models/OrbDatabaseContext.cs
+++ Models/OrbDatabaseContext.cs
@@ -17,10 +17,6 @@ public partial class OrbDatabaseContext : DbContext
 
     public virtual DbSet<Orb> Orbs { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlite("Data Source=Data/ORB_DATABASE.db");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Orb>(entity =>
